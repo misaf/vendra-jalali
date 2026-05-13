6104de4eb613febd4e6b7b88325dc365e6b488d9
Misaf <misaf.1990@gmail.com>
2026-05-13 13:07:43 +0330

    feat: add German translations

---
+++ resources/lang/de/days.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'short' => [
+        '1' => 'So',
+        '2' => 'Mo',
+        '3' => 'Di',
+        '4' => 'Mi',
+        '5' => 'Do',
+        '6' => 'Fr',
+        '7' => 'Sa',
+    ],
+    'long' => [
+        '1' => 'Sonntag',
+        '2' => 'Montag',
+        '3' => 'Dienstag',
+        '4' => 'Mittwoch',
+        '5' => 'Donnerstag',
+        '6' => 'Freitag',
+        '7' => 'Samstag',
+    ],
+];
